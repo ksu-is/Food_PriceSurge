f7bb5666fb438fdbbbe774fd39ad71c862d9772f
nickseitzz <nseitz@students.kennesaw.edu>
2022-04-21 15:11:39 -0400

    Wrote Initial Welcome Screen
    Began initial welcome screen for the program and added restaurant selection.

--- FoodSurge.py
+++ FoodSurge.py
@@ -4,4 +4,3 @@ input()
 while is_quitting == False:
     print("Select a restaurant to view prices: \n1. Mcdonalds\n2. Wendys\n3. Chick-Fil-A\n4. Cookout\n5. Quit")
     selection = input("Select a number from the list provided: ")
-
